7ab91d89e3b0f25b970ecbbfa9b44b49bea5ade3
Javier Pardo <javierpardollama@outlook.com>
2019-05-26 12:51:45 +0200

    CORRECCION INYECCION DEL MIDDLEWARE DE EXCEPCIONES

--- Sandwitch.Portal/Sandwitch.Tier.Web/Startup.cs
+++ Sandwitch.Portal/Sandwitch.Tier.Web/Startup.cs
@@ -58,14 +58,15 @@ namespace Sandwitch.Tier.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCustomExceptionMiddlewares();
             }
             else
             {
+                app.UseCustomExceptionMiddlewares();
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseCustomExceptionMiddlewares();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();

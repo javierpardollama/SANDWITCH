51dce0f793042d36f036af7b73a1f8aa7169aa7a
Javier Pardo <javierpardollama@outlook.com>
2019-07-12 22:50:02 +0200

    ACTUALIZACION PAQUETES NUGET

--- Sandwitch.Portal/Sandwitch.Tier.Entities/Interfaces/IBase.cs
+++ Sandwitch.Portal/Sandwitch.Tier.Entities/Interfaces/IBase.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Sandwitch.Tier.Entities.Interfaces
 {
     public interface IBase
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
         int Id { get; set; }
 
+        [Required]
         DateTime LastModified { get; set; }
 
+        [Required]
         bool Deleted { get; set; }
     }
 }

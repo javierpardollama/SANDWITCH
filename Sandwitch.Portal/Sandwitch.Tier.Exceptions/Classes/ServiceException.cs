51ff7fdca09108d757e375a1a3f13ee3d00d8ea1
Javier Pardo <javierpardollama@outlook.com>
2019-08-08 20:14:03 +0200

    NUEVA CAPTURA DE EXCEPCIONES GENERICAS

--- Sandwitch.Portal/Sandwitch.Tier.Exceptions/Classes/ServiceException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Sandwitch.Tier.Exceptions.Classes
-{
-    public class ServiceException : Exception
-    {
-        public ServiceException(string message) : base(message) { }
-    }
-}

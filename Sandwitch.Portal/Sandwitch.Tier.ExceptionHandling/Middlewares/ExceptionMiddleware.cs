747681cee5be516ddfdf0303c00f3b712d2f1e11
Javier Pardo <javierpardollama@outlook.com>
2020-04-21 15:36:33 +0200

    MEJORA EN SERIALIZACION DE EXCEPCIONES

--- Sandwitch.Portal/Sandwitch.Tier.ExceptionHandling/Middlewares/ExceptionMiddleware.cs
+++ Sandwitch.Portal/Sandwitch.Tier.ExceptionHandling/Middlewares/ExceptionMiddleware.cs
@@ -40,7 +40,7 @@ namespace Sandwitch.Tier.ExceptionHandling.Middlewares
                 Message = exception.Message
             };
 
-            return context.Response.WriteAsync(JsonSerializer.Serialize(viewException));
+            return context.Response.WriteAsync(JsonSerializer.Serialize(viewException, new JsonSerializerOptions() { WriteIndented = true }));
         }
     }
 }

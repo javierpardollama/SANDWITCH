9d1d2eab80beae3e353772318152fba8cc7d5b19
Javier Pardo <javierpardollama@outlook.com>
2022-08-10 17:14:05 +0200

    AUTENTICACION EN METRICAS

--- Sandwitch.Service/Sandwitch.Tier.Benchmarks/Services/PoblacionService.cs
+++ Sandwitch.Service/Sandwitch.Tier.Benchmarks/Services/PoblacionService.cs
@@ -5,7 +5,6 @@ using Sandwitch.Tier.ViewModels.Classes.Views;
 
 using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
@@ -15,9 +14,7 @@ namespace Sandwitch.Tier.Benchmarks.Services
 {
     public class PoblacionService
     {
-        static readonly NetworkCredential Credentials = new("Peach", "T/R4J6eyvNG<6ne!");
-        static readonly HttpClientHandler Handler = new() { Credentials = Credentials };
-        static readonly HttpClient Client = new(Handler) { BaseAddress = new Uri("https://localhost:7297/api/poblacion/") };
+        static readonly HttpClient Client = new() { BaseAddress = new Uri("https://localhost:7297/api/poblacion/") };
 
         [Benchmark]
         public async Task<IList<ViewPoblacion>> FindAllPoblacion()

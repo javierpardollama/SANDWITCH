499c4c78bf186b2f09d5eee6b8f30b616e5d73c7
Javier Pardo <javierpardollama@outlook.com>
2024-02-08 19:06:44 +0100

    DOCUMENTACION DE CODIGO

--- Sandwitch.Service/Sandwitch.Tier.Helpers/Classes/AsyncHelper.cs
+++ Sandwitch.Service/Sandwitch.Tier.Helpers/Classes/AsyncHelper.cs
@@ -5,13 +5,26 @@ using System.Threading.Tasks;
 
 namespace Sandwitch.Tier.Helpers.Classes
 {
+    /// <summary>
+    /// Represents a <see cref="AsyncHelper"/> class.
+    /// </summary>
     public static class AsyncHelper
     {
+
+        /// <summary>
+        /// Instance of <see cref="TaskFactory"/>
+        /// </summary>
         private static readonly TaskFactory TaskFactory = new TaskFactory(CancellationToken.None,
                                                                           TaskCreationOptions.None,
                                                                           TaskContinuationOptions.None,
                                                                           TaskScheduler.Default);
 
+        /// <summary>
+        /// Runs Tasks Synchronously
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="func">Injected <see cref="Func{T, TResult}"/></param>
+        /// <returns>Instance of <see cref="{TResult}"/></returns>
         public static TResult RunSync<TResult>(Func<Task<TResult>> func)
         {
             var cultureUi = CultureInfo.CurrentUICulture;
@@ -27,6 +40,10 @@ namespace Sandwitch.Tier.Helpers.Classes
             .GetResult();
         }
 
+        /// <summary>
+        /// Runs Tasks Synchronously
+        /// </summary>
+        /// <param name="func">Injected <see cref="Func{T, TResult}"/></param>
         public static void RunSync(Func<Task> func)
         {
             var cultureUi = CultureInfo.CurrentUICulture;

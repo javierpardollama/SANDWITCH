67b35402f68387005696173a858f03b44a314193
Javier Pardo <javierpardollama@outlook.com>
2024-04-17 14:57:51 +0200

    SELENIUM TESTS

---
+++ Sandwitch.Service/Sandwitch.Tier.Client.Tests/Classes/TestHome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+using NUnit.Framework;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace Sandwitch.Tier.Client.Tests.Classes
+{
+    public class TestHome
+    {
+        private IWebDriver Driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {            
+            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+            Driver = new ChromeDriver(path + @"\Drivers");
+        }
+
+        [Test]
+        public void Index()
+        {
+            Driver.Navigate().GoToUrl("https://localhost:4200");
+
+            Assert.That(Driver.Title.Contains(""), Is.True);
+        }      
+
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}  

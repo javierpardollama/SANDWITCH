67e543bc04e825e31b939f95f4982b76371295f4
Javier Pardo <javierpardollama@outlook.com>
2024-04-19 14:00:34 +0200

    SELENIUM TESTS

---
+++ Sandwitch.Service/Sandwitch.Tier.Client.Tests/Classes/TestArenales.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Sandwitch.Tier.Client.Tests.Classes
+{
+    [TestFixture]
+    public class TestArenales
+    {
+        private ChromeDriver Driver;
+        private WebDriverWait Wait;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+
+            Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20))
+            {
+                PollingInterval = TimeSpan.FromMilliseconds(200)
+            };
+
+            Driver.Navigate().GoToUrl("https://localhost:4200");
+        }
+
+        [Test]
+        public void Add()
+        {
+            Wait.Until(d => d.FindElement(By.Id("nav-arenales"))).Click();
+
+            Wait.Until(d => d.FindElement(By.ClassName("add-button"))).Click();
+
+            Wait.Until(d => d.FindElement(By.Id("arenal-add-modal")).Displayed);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
--- Sandwitch.Service/Sandwitch.Tier.Client.Tests/Classes/TestNavigation.cs
+++ Sandwitch.Service/Sandwitch.Tier.Client.Tests/Classes/TestNavigation.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+
+using NUnit.Framework;
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -6,29 +8,31 @@ using OpenQA.Selenium.Chrome;
 
 namespace Sandwitch.Tier.Client.Tests.Classes
 {
+    [TestFixture]
     public class TestNavigation
     {
         private ChromeDriver Driver;
 
         [OneTimeSetUp]
         public void Setup()
-        {           
+        {
             Driver = new ChromeDriver();
-           
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            Driver.Manage().Window.Maximize();
+
             Driver.Navigate().GoToUrl("https://localhost:4200");
         }
 
         [Test]
         public void Home()
-        {           
+        {
             Assert.That(Driver.Title.Contains("Sandwitch"), Is.True);
         }
 
         [Test]
         public void Provincias()
         {
-            var @elememt = Driver.FindElement(By.Id("nav-provincias"));
-            @elememt.Click();
+            Driver.FindElement(By.Id("nav-provincias")).Click();
 
             Assert.That(Driver.Url.Contains("provincias"), Is.True);
         }
@@ -36,8 +40,7 @@ namespace Sandwitch.Tier.Client.Tests.Classes
         [Test]
         public void Poblaciones()
         {
-            var @elememt = Driver.FindElement(By.Id("nav-poblaciones"));
-            @elememt.Click();
+            Driver.FindElement(By.Id("nav-poblaciones")).Click();
 
             Assert.That(Driver.Url.Contains("poblaciones"), Is.True);
         }
@@ -45,8 +48,7 @@ namespace Sandwitch.Tier.Client.Tests.Classes
         [Test]
         public void Arenales()
         {
-            var @elememt = Driver.FindElement(By.Id("nav-arenales"));
-            @elememt.Click();
+            Driver.FindElement(By.Id("nav-arenales")).Click();
 
             Assert.That(Driver.Url.Contains("arenales"), Is.True);
         }
@@ -54,8 +56,8 @@ namespace Sandwitch.Tier.Client.Tests.Classes
         [Test]
         public void Banderas()
         {
-            var @elememt = Driver.FindElement(By.Id("nav-banderas"));
-            @elememt.Click();
+            Driver.FindElement(By.Id("nav-banderas")).Click(); ;
+
 
             Assert.That(Driver.Url.Contains("banderas"), Is.True);
         }
@@ -67,4 +69,4 @@ namespace Sandwitch.Tier.Client.Tests.Classes
             Driver.Quit();
         }
     }
-}  
+}

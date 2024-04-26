3c32544d81d8424eac63507f13549223d4464e90
Javier Pardo <javierpardollama@outlook.com>
2024-04-26 18:21:10 +0200

    CORRECCION DE TESTS

--- Sandwitch.Service/Sandwitch.Tier.Helpers.Test/Classes/SequenceHelperTest.cs
+++ Sandwitch.Service/Sandwitch.Tier.Helpers.Test/Classes/SequenceHelperTest.cs
@@ -15,16 +15,8 @@ namespace Sandwitch.Tier.Helpers.Test.Classes
         [TestCase(new object[] { 0, 1 }, 2)]
         [TestCase(new object[] { 0 }, 1)]
         [TestCase(new object[] { }, 0)]       
-        [TestCase(new object[] { 0, 1, 2, 6 }, 3)]
-        [TestCase(new object[] { 0, 1 }, 2, 0)]
-        [TestCase(new object[] { 0 }, 1, 0)]
-        [TestCase(new object[] { }, 0, 0)]
-        [TestCase(new object[] { 0, 1, 2, 6 }, 3, 0)]
-        [TestCase(new object[] { 0, 1 }, 2, 1)]
-        [TestCase(new object[] { 0 }, 1, 1)]
-        [TestCase(new object[] { }, 0, 1)]
-        [TestCase(new object[] { 0, 1, 2, 6 }, 3, 1)]
-        public void GetNextAvailableNumber(object[] given, int expected, int offset = 0)
+        [TestCase(new object[] { 0, 1, 2, 6 }, 3)]     
+        public void GetNextAvailableNumber(object[] given, int expected)
         {
             var result = SequenceHelper.GetNextAvailableNumber(given.Cast<int>().ToList());
 

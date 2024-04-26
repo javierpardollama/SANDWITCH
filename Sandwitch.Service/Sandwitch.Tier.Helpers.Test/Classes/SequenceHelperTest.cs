0fcc4584742250d6f41b93d15cbc1d32848d2fd3
Javier Pardo <javierpardollama@outlook.com>
2024-04-26 18:16:13 +0200

    CORRECCION DE TESTS

--- Sandwitch.Service/Sandwitch.Tier.Helpers.Test/Classes/SequenceHelperTest.cs
+++ Sandwitch.Service/Sandwitch.Tier.Helpers.Test/Classes/SequenceHelperTest.cs
@@ -3,6 +3,7 @@
 using Sandwitch.Tier.Helpers.Classes;
 
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Sandwitch.Tier.Helpers.Test.Classes
 {
@@ -11,147 +12,23 @@ namespace Sandwitch.Tier.Helpers.Test.Classes
     public class SequenceHelperTest
     {
         [Test]
-        public void When_Default_Sequence_Plus_Offset_Default_Then_GetNextAvailableNumber()
+        [TestCase(new object[] { 0, 1 }, 2)]
+        [TestCase(new object[] { 0 }, 1)]
+        [TestCase(new object[] { }, 0)]       
+        [TestCase(new object[] { 0, 1, 2, 6 }, 3)]
+        [TestCase(new object[] { 0, 1 }, 2, 0)]
+        [TestCase(new object[] { 0 }, 1, 0)]
+        [TestCase(new object[] { }, 0, 0)]
+        [TestCase(new object[] { 0, 1, 2, 6 }, 3, 0)]
+        [TestCase(new object[] { 0, 1 }, 2, 1)]
+        [TestCase(new object[] { 0 }, 1, 1)]
+        [TestCase(new object[] { }, 0, 1)]
+        [TestCase(new object[] { 0, 1, 2, 6 }, 3, 1)]
+        public void GetNextAvailableNumber(object[] given, int expected, int offset = 0)
         {
-            var given = new List<int>() { 0, 1 };
-
-            var expected = 2;
-
-            var result = SequenceHelper.GetNextAvailableNumber(given);
+            var result = SequenceHelper.GetNextAvailableNumber(given.Cast<int>().ToList());
 
             Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void When_Empty_Sequence_Plus_Offset_Default_Then_GetNextAvailableNumber()
-        {
-            var given = new List<int>() { };
-
-            var expected = 0;
-
-            var result = SequenceHelper.GetNextAvailableNumber(given);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void When_Null_Sequence_Plus_Offset_Default_Then_GetNextAvailableNumber()
-        {
-            List<int> given = null;
-
-            var expected = 0;
-
-            var result = SequenceHelper.GetNextAvailableNumber(given);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void When_Jump_Sequence_Plus_Offset_Default_Then_GetNextAvailableNumber()
-        {
-            var given = new List<int>() { 0, 1, 2, 6 };
-
-            var expected = 3;
-
-            var result = SequenceHelper.GetNextAvailableNumber(given);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void When_Default_Sequence_Plus_Offset_One_Then_GetNextAvailableNumber()
-        {
-            var given = new List<int>() { 0, 1 };
-
-            var expected = 2;
-
-            var result = SequenceHelper.GetNextAvailableNumber(given, 1);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void When_Empty_Sequence_Plus_Offset_One_Then_GetNextAvailableNumber()
-        {
-            var given = new List<int>() { };
-
-            var expected = 1;
-
-            var result = SequenceHelper.GetNextAvailableNumber(given, 1);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void When_Null_Sequence_Plus_Offset_One_Then_GetNextAvailableNumber()
-        {
-            List<int> given = null;
-
-            var expected = 1;
-
-            var result = SequenceHelper.GetNextAvailableNumber(given, 1);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void When_Jump_Sequence_Plus_Offset_One_Then_GetNextAvailableNumber()
-        {
-            var given = new List<int>() { 0, 1, 2, 6 };
-
-            var expected = 3;
-
-            var result = SequenceHelper.GetNextAvailableNumber(given, 1);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void When_Default_Sequence_Plus_Offset_Zero_Then_GetNextAvailableNumber()
-        {
-            var given = new List<int>() { 0, 1 };
-
-            var expected = 2;
-
-            var result = SequenceHelper.GetNextAvailableNumber(given, 0);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void When_Empty_Sequence_Plus_Offset_Zero_Then_GetNextAvailableNumber()
-        {
-            var given = new List<int>() { };
-
-            var expected = 0;
-
-            var result = SequenceHelper.GetNextAvailableNumber(given, 0);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void When_Null_Sequence_Plus_Offset_Zero_Then_GetNextAvailableNumber()
-        {
-            List<int> given = null;
-
-            var expected = 0;
-
-            var result = SequenceHelper.GetNextAvailableNumber(given, 0);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void When_Jump_Sequence_Plus_Offset_Zero_Then_GetNextAvailableNumber()
-        {
-            var given = new List<int>() { 0, 1, 2, 6 };
-
-            var expected = 3;
-
-            var result = SequenceHelper.GetNextAvailableNumber(given, 0);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
+        }      
     }
 }

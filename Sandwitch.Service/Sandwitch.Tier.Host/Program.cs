4a357d0ec1065d97545a398280406ec311fd4137
Javier Pardo <javierpardollama@outlook.com>
2024-11-22 11:46:21 +0100

    BORRADO DE ASPIRE HOST

--- Sandwitch.Service/Sandwitch.Tier.Host/Program.cs
+++
@@ -1,9 +0,0 @@
-using Aspire.Hosting;
-
-var builder = DistributedApplication.CreateBuilder(args);
-
-var service = builder.AddProject<Projects.Sandwitch_Tier_Service>("service");
-
-var client = builder.AddNpmApp("client", "./../../Sandwitch.Client");
-
-builder.Build().Run();

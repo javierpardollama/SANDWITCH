0e3e441dfa9d1607eb6e2524c3dd8927c7465fdb
Javier Pardo <javierpardollama@outlook.com>
2025-01-16 10:02:48 +0100

    MIGRACION A LAZYLOADING

--- Sandwitch.Client/src/app/app.routes.ts
+++ Sandwitch.Client/src/app/app.routes.ts
@@ -1,51 +1,43 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ArenalGridComponent } from './management/grids/arenal-grid/arenal-grid.component';
-import { BanderaGridComponent } from './management/grids/bandera-grid/bandera-grid.component';
-import { PoblacionGridComponent } from './management/grids/poblacion-grid/poblacion-grid.component';
-import { ProvinciaGridComponent } from './management/grids/provincia-grid/provincia-grid.component';
-import { VientoGridComponent } from './management/grids/viento-grid/viento-grid.component';
-import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
-import { UnknownComponent } from './unknown/unknown.component';
 
 export const routes: Routes = [
-    {
-      path: '',
-      component: HomeComponent,
-      pathMatch: 'full'
-    },
-    {
-      path: 'management/vientos',
-      component: VientoGridComponent,
-      pathMatch: 'full'
-    },
-    {
-      path: 'management/banderas',
-      component: BanderaGridComponent,
-      pathMatch: 'full'
-    },
-    {
-      path: 'management/poblaciones',
-      component: PoblacionGridComponent,
-      pathMatch: 'full'
-    },
-    {
-      path: 'management/provincias',
-      component: ProvinciaGridComponent,
-      pathMatch: 'full'
-    },
-    {
-      path: 'management/arenales',
-      component: ArenalGridComponent,
-      pathMatch: 'full'
-    },
-    {
-      path: 'unknown',
-      component: UnknownComponent,
-      pathMatch: 'full'
-    },
-    {
-      path: 'unauthorized',
-      component: UnauthorizedComponent,
-      pathMatch: 'full'
-    }];
+  {
+    path: '',
+    loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
+    pathMatch: 'full'
+  },
+  {
+    path: 'management/vientos',
+    loadComponent: () => import('./management/grids/viento-grid/viento-grid.component').then((m) => m.VientoGridComponent),
+    pathMatch: 'full'
+  },
+  {
+    path: 'management/banderas',
+    loadComponent: () => import('./management/grids/bandera-grid/bandera-grid.component').then((m) => m.BanderaGridComponent),
+    pathMatch: 'full'
+  },
+  {
+    path: 'management/poblaciones',
+    loadComponent: () => import('./management/grids/poblacion-grid/poblacion-grid.component').then((m) => m.PoblacionGridComponent),
+    pathMatch: 'full'
+  },
+  {
+    path: 'management/provincias',
+    loadComponent: () => import('./management/grids/provincia-grid/provincia-grid.component').then((m) => m.ProvinciaGridComponent),
+    pathMatch: 'full'
+  },
+  {
+    path: 'management/arenales',
+    loadComponent: () => import('./management/grids/arenal-grid/arenal-grid.component').then((m) => m.ArenalGridComponent),
+    pathMatch: 'full'
+  },
+  {
+    path: 'unknown',
+    loadComponent: () => import('./unknown/unknown.component').then((m) => m.UnknownComponent),
+    pathMatch: 'full'
+  },
+  {
+    path: 'unauthorized',
+    loadComponent: () => import('./unauthorized/unauthorized.component').then((m) => m.UnauthorizedComponent),
+    pathMatch: 'full'
+  }];

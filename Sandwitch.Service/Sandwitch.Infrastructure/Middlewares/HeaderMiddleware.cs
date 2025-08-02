710c6471a2439754395e4b63a26941b4ef5b0496
Javier Pardo <javierpardollama@outlook.com>
2025-08-02 16:21:32 +0200

    ACTUALIZACION DE PAQUETES NUGET

--- Sandwitch.Service/Sandwitch.Infrastructure/Middlewares/HeaderMiddleware.cs
+++ Sandwitch.Service/Sandwitch.Infrastructure/Middlewares/HeaderMiddleware.cs
@@ -7,24 +7,24 @@ namespace Sandwitch.Infrastructure.Middlewares;
 ///     Represents a <see cref="HeaderMiddleware" /> class
 /// </summary>
 /// <param name="request">Injected <see cref="RequestDelegate" /></param>
-public class HeaderMiddleware(RequestDelegate request)
+public class HeaderMiddleware(RequestDelegate @request)
 {
     /// <summary>
     ///     Handles Requests Asynchronously
     /// </summary>
     /// <param name="context">Injected <see cref="HttpContext" /></param>
     /// <returns>Instance of <see cref="Task" /></returns>
-    public async Task InvokeAsync(HttpContext context)
+    public async Task InvokeAsync(HttpContext @context)
     {
-        context.Response.Headers.Remove("X-Powered-By");
-        context.Response.Headers.Remove("Server");
-        context.Response.Headers.XContentTypeOptions = new StringValues("nosniff");
-        context.Response.Headers.XFrameOptions = new StringValues("deny");
-        context.Response.Headers.XXSSProtection = new StringValues("0");
-        context.Response.Headers.ContentSecurityPolicy = new StringValues("default-src 'self'");
+        @context.Response.Headers.Remove("X-Powered-By");
+        @context.Response.Headers.Remove("Server");
+        @context.Response.Headers.XContentTypeOptions = new StringValues("nosniff");
+        @context.Response.Headers.XFrameOptions = new StringValues("deny");
+        @context.Response.Headers.XXSSProtection = new StringValues("0");
+        @context.Response.Headers.ContentSecurityPolicy = new StringValues("default-src 'self'");
         
-        @context.Response.Headers.Append("X-Clacks-Overhead", "GNU Terry Pratchett");
+        @context.Response.Headers.Append("X-Clacks-Overhead", new StringValues("GNU Terry Pratchett"));
 
-        await request(context);
+        await @request(@context);
     }
 }

c710ace505cee2fe8c3c1ab31d31ea0f7a272190
Javier Pardo <javierpardollama@outlook.com>
2025-10-28 08:56:19 +0100

    LIMPIEZA DE CODIGO

--- Sandwitch.Service/Sandwitch.Service/Program.cs
+++ Sandwitch.Service/Sandwitch.Service/Program.cs
@@ -2,7 +2,6 @@ using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Sandwitch.Application.Installers;
 using Sandwitch.Domain.Settings;
 using Sandwitch.Infrastructure.Installers;

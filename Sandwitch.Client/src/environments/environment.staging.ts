6f1ac491a69b4bb1a7a900c4b1a8fc3de0bed6a4
Javier Pardo <javierpardollama@outlook.com>
2025-11-17 13:39:46 +0100

    ENTORNO STAGING EN CLIENTE

---
+++ Sandwitch.Client/src/environments/environment.staging.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  Api:
+  {
+    Lock: "Rosalina",
+    Key: "uPu}S}*8q(S{sCgc",
+    Service:"https://localhost:7297/",
+  }, 
+  Otel:
+  {
+    Exporter:"https://localhost:21023/v1/traces",
+    Key:"04adc0ddb20983d0e82b171afdaf2fd8"
+  },
+  ServiceWorker:
+  {
+    Enabled: true,
+    TimeOut: 30000
+  },
+  production: true
+};

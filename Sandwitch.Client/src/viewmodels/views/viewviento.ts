798ca4a949055f7675239f0dabdeed3f47aee677
Javier Pardo <javierpardollama@outlook.com>
2025-12-11 08:26:06 +0100

    ACTUALIZACION DE PAQUETES NUGET

--- Sandwitch.Client/src/viewmodels/views/viewviento.ts
+++ Sandwitch.Client/src/viewmodels/views/viewviento.ts
@@ -1,8 +1,6 @@
 import { ViewBase } from './viewbase';
-import { ViewHistorico } from './viewhistorico';
 
 export interface ViewViento extends ViewBase {
     ImageUri: string;
-    Name: string;
-    Historicos: ViewHistorico[];
+    Name: string;   
 }

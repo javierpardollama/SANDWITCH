80d92025bf7d8b58355ffd1c321fd43a11543d88
Javier Pardo <javierpardollama@outlook.com>
2025-12-30 15:37:26 +0100

    MIGRACION INTERCEPTOR ANGULAR 21

--- Sandwitch.Client/src/app/app.config.ts
+++ Sandwitch.Client/src/app/app.config.ts
@@ -1,18 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { AuthInterceptor } from '../interceptors/auth.interceptor';
 import { provideServiceWorker } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AuthInterceptor } from 'src/interceptors/auth.interceptor';
 
 export const AppConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(routes),   
+    provideHttpClient(withInterceptors([AuthInterceptor])),
     provideServiceWorker('ngsw-worker.js',
         {
             enabled: environment.ServiceWorker.Enabled,
--- Sandwitch.Client/src/interceptors/auth.interceptor.ts
+++ Sandwitch.Client/src/interceptors/auth.interceptor.ts
@@ -1,17 +1,12 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        request = request.clone({
-            setHeaders: {
-                Authorization: `Basic ${window.btoa(environment.Api.Lock + ':' + environment.Api.Key)}`
-            }
-        });
-        
-        return next.handle(request);
-    }
-}
+export const AuthInterceptor: HttpInterceptorFn = (request, next) => {
+    request = request.clone({
+        setHeaders: {
+            Authorization: `Basic ${window.btoa(environment.Api.Lock + ':' + environment.Api.Key)}`
+        },
+    });
+
+    return next(request);
+};
